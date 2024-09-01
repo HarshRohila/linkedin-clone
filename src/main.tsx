34eba9a65aabc30e0748c507ce3a8885e6a502ba
Harsh Rohila <rohilaharsh@gmail.com>
2024-09-01 13:56:47 +0530

    installed material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { makeServer } from './mock-server/index.ts'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { makeServer } from "./mock-server/index.ts"
+import "./material-styles.ts"
 
 makeServer({ environment: "development" })
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
